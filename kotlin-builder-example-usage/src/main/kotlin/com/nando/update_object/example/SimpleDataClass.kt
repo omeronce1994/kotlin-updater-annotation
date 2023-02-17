4a1379d07338c8cc3c30d6b4fa8ebe7e15604edc
omercohen <omeronce1994@gmail.com>
2023-02-17 12:59:44 +0200

    Added class name option for UpdateObject.kt annotation. Added field annotation RequiredForUpdateObject.kt to mark field as required for generated update object

--- kotlin-builder-example-usage/src/main/kotlin/com/nando/update_object/example/SimpleDataClass.kt
+++ kotlin-builder-example-usage/src/main/kotlin/com/nando/update_object/example/SimpleDataClass.kt
@@ -1,11 +1,13 @@
 package com.nando.update_object.example
 
+import com.nando.update_object.annotation.RequiredForUpdateObject
 import com.nando.update_object.annotation.UpdateObject
 import com.nando.update_object.annotation.modifiers.UpdateObjectModifier
 import java.time.LocalDate
 
 @UpdateObject(visibilityModifiers = [UpdateObjectModifier.VisibilityModifier.Internal::class])
 data class SimpleDataClass(
+        @RequiredForUpdateObject
         val notNullString: String,
         val nullableString: String?,
         val notNullLong: Long,
